730fec1c453c8568682c5cb3220d808b73c39bb2
ph-alaa <Alaa_lafi333@hotmail.com>
2023-07-03 14:20:48 +0300

    Lab07

---
+++ ZOO2/Classes/Bird.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZOO2.Classes;
+using ZOO2.Interfaces;
+
+namespace ZOO2.Classes
+{
+    public class Bird : Animal
+    {
+        public string FeatherColor { get; set; }
+        public bool CanFly { get; set; }
+
+        public Bird(string name, int age, string featherColor, bool canFly) : base(name, age)
+        {
+            FeatherColor = featherColor;
+            CanFly = canFly;
+        }
+
+        public virtual string Eat()
+        {
+            return "The bird is eating.";
+        }
+    }
+    public class Eagle : Bird, IAttack, ISpeed
+    {
+        public double AvgSpeed { get; set; }
+
+        public Eagle(string name, int age, string featherColor, bool canFly) : base(name, age, featherColor, canFly)
+        {
+            AvgSpeed = 80.0;
+        }
+
+        public override string Eat()
+        {
+            return "The eagle is eating fish.";
+        }
+
+
+
+        public string WhenAttack()
+        {
+            return "The eagle swoops down and attacks its prey with its sharp talons.";
+        }
+
+        public string GetSpeed()
+        {
+            return $"The average speed of the eagle is {AvgSpeed} mph.";
+        }
+
+    }
+    public class Parrot : Bird
+    {
+        public Parrot(string name, int age, string featherColor, bool canFly) : base(name, age, featherColor, canFly)
+        {
+        }
+
+        public override string Eat()
+        {
+            return "The parrot is eating seeds and fruits.";
+        }
+    }
+}
---
+++ ZOO2/Classes/Reptile.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZOO2.Classes;
+using ZOO2.Interfaces;
+
+namespace ZOO2.Classes
+{
+    public class Reptile : Animal
+    {
+        public bool HasScales { get; set; }
+
+        public Reptile(string name, int age, bool hasScales) : base(name, age)
+        {
+            HasScales = hasScales;
+
+        }
+
+        public virtual string Snap()
+        {
+            return "The reptile is Snap.";
+        }
+    }
+    public class Crocodile : Reptile, IAttack, ISpeed
+    {
+        public string JawSize { get; set; }
+        public double AvgSpeed { get; set; }
+
+        public Crocodile(string name, int age, bool hasScales, string jawSize) : base(name, age, hasScales)
+        {
+            JawSize = jawSize;
+            AvgSpeed = 20.0;
+        }
+
+        public override string Snap()
+        {
+            return "The crocodile snaps its jaws.";
+        }
+
+        public string GetSpeed()
+        {
+            return $"The crocodile has an average swimming speed of {AvgSpeed} mph.";
+        }
+
+        public string WhenAttack()
+        {
+            return "The crocodile launches a powerful bite!";
+        }
+    }
+
+
+
+}
